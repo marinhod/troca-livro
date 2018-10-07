6d56c61e20b3c6596e7afee2c215dcc169c5be6f
Diego Marinho <marinhop.diego@gmail.com>
2018-10-07 16:20:57 -0300

    feat: added livro-lista component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { LivroListaComponent } from './livro-lista/livro-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivroListaComponent
   ],
   imports: [
     BrowserModule,
