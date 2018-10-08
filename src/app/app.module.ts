422700ceb0fd13d76e744d5499b74999bbac653b
Diego Marinho <marinhop.diego@gmail.com>
2018-10-08 15:44:22 -0300

    refactor: changed pages/ name to components/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { appRoutes } from './routes';
-import { UsuarioComponent } from './pages/usuario/usuario.component';
-import { LivroComponent } from './pages/livro/livro.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { LivroComponent } from './components/livro/livro.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
-import { UsuarioComponent } from './pages/usuario/usuario.component';
-import { LivroComponent } from './pages/livro/livro.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { LivroComponent } from './components/livro/livro.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent },
