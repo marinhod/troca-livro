44cef0155e5a139a3e85f18852ee8d50bb211cbb
Diego Marinho <marinhop.diego@gmail.com>
2018-10-10 14:40:59 -0300

    feat: added livro service

---
+++ src/app/services/livro.service.spec.ts
@@ -0,0 +1,11 @@
+import { TestBed } from '@angular/core/testing';
+import { LivroService } from './livro.service';
+
+describe('LivroService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: LivroService = TestBed.get(LivroService);
+    expect(service).toBeTruthy();
+  });
+});
