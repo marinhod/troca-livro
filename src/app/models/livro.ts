0e6f9d55da07c9c8d840b9e4d5beb0f56a061952
Diego Marinho <marinhop.diego@gmail.com>
2018-10-10 15:03:51 -0300

    feat: added slug to usuario and livro models

--- src/app/models/livro.ts
+++ src/app/models/livro.ts
@@ -4,6 +4,7 @@ import { Genero } from "./genero";
 export class Livro {
     id: string;
     titulo: string;
+    slug: string;
     autor: Autor[];
     genero: Genero[];
     ano: Date;
--- src/app/models/usuario.ts
+++ src/app/models/usuario.ts
@@ -3,6 +3,7 @@ import { Livro } from "./livro";
 export class Usuario {
     nome: string;
     sobrenome: string;
+    slug: string;
     email: string;
     livros: Livro[];
 }
