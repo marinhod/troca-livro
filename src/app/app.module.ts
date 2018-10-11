cddc02f4e45157c4033867fc924535ba2eb921e2
Diego Marinho <marinhop.diego@gmail.com>
2018-10-11 13:52:14 -0300

    feat: added http import to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { appRoutes } from './routes';
+import { HttpClientModule } from '@angular/common/http';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { LivroComponent } from './components/livro/livro.component';
 import { HomeComponent } from './components/home/home.component';
@@ -20,7 +21,8 @@ import { LoginComponent } from './components/login/login.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot( appRoutes, { enableTracing: true } )
+    RouterModule.forRoot( appRoutes, { enableTracing: true } ),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
