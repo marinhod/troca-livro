d8a9533542ccf12de2187204f321cff14617d8ca
Diego Marinho <marinhop.diego@gmail.com>
2018-10-12 16:25:15 -0300

    feat: added get subscription to livro component

--- src/app/components/livro/livro.component.ts
+++ src/app/components/livro/livro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Livro } from '../../models/livro';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { LivroService } from '../../services/livro.service';
 
 @Component({
   selector: 'app-livro',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./livro.component.css']
 })
 export class LivroComponent implements OnInit {
+  private livro: Livro;
+  private error: any;
+  private slug: string;
+  private sub: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute, 
+    private livroService: LivroService) {}
+  
+  getLivro(slug: string): void {
+    this.livroService
+      .get(slug)
+      .subscribe(
+        item => (this.livro = item),
+        error => (this.error = error)
+      )
+  }
+
+  getSlug(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.slug = params['slug'];
+    });
+  }
 
   ngOnInit() {
+    this.getSlug();
+    this.getLivro(this.slug);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
