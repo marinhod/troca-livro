9fafd5de9fd62a3d48dc8646bb4b242b438af3fd
Diego Marinho <marinhop.diego@gmail.com>
2018-10-12 15:59:53 -0300

    feat: added get function to usuario service

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private URL = 'http://localhost:3000/api/v1/usuario';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get(slug: string): Observable<Usuario> {
+    let url: string = `${this.URL}/${slug}`;
+    return this.http.get<Usuario>(url);
+  }
+
 }
