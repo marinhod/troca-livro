56b26fb4acc628800abaa90d80119542f37ed19d
Diego Marinho <marinhop.diego@gmail.com>
2018-10-12 16:02:16 -0300

    feat: added subscription to usuario service get in usuario component

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Usuario } from '../../models/usuario';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent implements OnInit {
+export class UsuarioComponent implements OnInit, OnDestroy {
+  private usuario: Usuario;
+  private error: any;
+  private slug: string;
+  private sub: any;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute, 
+    private usuarioService: UsuarioService) {}
+  
+  getUsuario(slug: string): void {
+    this.usuarioService
+      .get(slug)
+      .subscribe(
+        item => (this.usuario = item),
+        error => (this.error = error)
+      )
+  }
+
+  getSlug(): void {
+    this.sub = this.route.params.subscribe(params => {
+      this.slug = params['slug'];
+    });
+  }
 
   ngOnInit() {
+    this.getSlug();
+    this.getUsuario(this.slug);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 
 }
