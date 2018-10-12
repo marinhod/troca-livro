e7e98b350fd86809b9bc004483620dd06173fa17
Diego Marinho <marinhop.diego@gmail.com>
2018-10-12 16:17:36 -0300

    feat: added get function to livro service

--- src/app/services/livro.service.ts
+++ src/app/services/livro.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Livro } from '../models/livro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LivroService {
+  private URL = 'http://localhost:3000/api/v1/livro';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get(slug: string): Observable<Livro> {
+    let url: string = `${this.URL}/${slug}`;
+    return this.http.get<Livro>(url);
+  }
+
 }
